d237afbb509168339af51806045bce9aedb7d1fb
rahulthagadur <rt_rahul@hotmail.com>
2017-10-28 00:28:43 +0530

    Small \\changes

--- app/src/main/java/com/example/thagadur/imdbmovieapp/MainActivity.java
+++ app/src/main/java/com/example/thagadur/imdbmovieapp/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.thagadur.imdbmovieapp;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -10,23 +10,28 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView movieRecyclerView;
     Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initialisation of Layout Items
+        initialisatonOfID();
 
-        movieRecyclerView=(RecyclerView)findViewById(R.id.movie_recyler_view);
-        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         movieRecyclerView.setLayoutManager(layoutManager);
-        CustomMovieListAdapter customMovieListAdapter=new CustomMovieListAdapter(context);
+        CustomMovieListAdapter customMovieListAdapter = new CustomMovieListAdapter(context);
         movieRecyclerView.setAdapter(customMovieListAdapter);
+    }
+
+    public void initialisatonOfID() {
+
+        movieRecyclerView = (RecyclerView) findViewById(R.id.movie_recyler_view);
     }
 
     @Override
     protected void onStart() {
-
-
         super.onStart();
     }
 }
